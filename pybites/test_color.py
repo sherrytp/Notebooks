5098251603a1925b230b21dae889ee20a78f89e8
Sherry <52416649+sherrytp@users.noreply.github.com>
2023-02-09 21:42:09 -0800

    Add files via upload

---
+++ pybites/test_color.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+
+from colors import print_colors
+
+NOT_VALID = 'Not a valid color'
+
+
+def call_print_colors(): 
+    try: 
+        print_colors()
+    except SystemExit: 
+        pass
+
+
+@patch("builtins.input", side_effect=['blue', 'white', 'quit'])
+def test_valid_then_invalid_color_then_quit(input_mock, capsys):
+    call_print_colors()
+    actual = capsys.readouterr()[0].strip()
+    expected = f'blue\n{NOT_VALID}\nbye'
+    assert actual == expected 
+
+
+@patch("builtins.input", side_effect=['green', 'quit'])
+def test_invalid_color_then_quit(input_mock,capsys): 
+    call_print_colors()
+    actual = capsys.readouterr()[0].strip()
+    expected = f'{NOT_VALID}\nbye'
+    assert actual == expected 
+
---
+++ pybites/test_winners.py
@@ -0,0 +1,13 @@
+from winners import GAME_STATS, print_game_stats
+
+def test_print_game_stats(capfd): 
+    prints = ["sara has won 0 games", 
+            "bob has won 1 game", 
+            "tim has won 5 games", 
+            "julian has won 3 games", 
+            "jim has won 1 game"]
+    print_game_stats(GAME_STATS) 
+    output = capfd.readouterr()[0].strip()
+
+    for line in prints: 
+        assert line in output
